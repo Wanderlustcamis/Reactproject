7133ee77ae94706c442cd43f341186aa07e22c10
wanderlustcamis <germanophile@gmail.com>
2021-02-26 20:36:36 -0300

    first exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    let c= ['Chocolate con Leche, Chocolate negro, Chocolate blanco.'];
+    let g =[' Chocolate de reposteria libres de gluten.'];
+    let i =[ " Informate!"];
+    let mix = c.concat(g);
+    console.log(mix + i);
+
